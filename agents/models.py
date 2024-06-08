e724ef53b5dcaeaf887decc4e0ea45fba1fbf58a
Abedif <fatemehabedi5219@gmail.com>
2024-06-09 02:17:23 +0330

    models

--- agents/models.py
+++ agents/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
+class Agents(models.Model):
+    title = models.CharField(max_length=220)
+    image = models.ImageField()
+    user = models.ForeignKey()
+    score = models.IntegerField()
+    description = models.TextField()
+    phone = models.IntegerField()
+    email = models.EmailField()
+    facebook = models.CharField(max_length=300)
+    x = models.CharField(max_length=300)
+    instagram = models.CharField(max_length=300)
+    linkedin = models.CharField(max_length=300)
+
+
+
+
+
+
+
+
 # Create your models here.
--- property/models.py
+++ property/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
+class category_property(models.Model):
+    title = models.CharField(max_length=220)
+    status = models.BooleanField(default=True)
+
+
+class property(models.Model):
+    title = models.CharField(max_length=220)
+    image = models.ImageField()
+    agent = models.ForeignKey()
+    category = models.ForeignKey(category_property)
+    rent = models.IntegerField()
+    area = models.IntegerField()
+    beds = models.IntegerField()
+    baths = models.IntegerField()
+    garages = models.IntegerField()
+    view = models.IntegerField()
+
+
+
+
+
+
+
 # Create your models here.
